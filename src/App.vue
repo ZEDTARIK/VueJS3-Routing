d0f9affeb364d3c0bd5eb40f04543bf2646a272d
ETTARAK <ettarak.zouhair@gmail.com>
2021-01-18 17:24:49 +0100

    Coding, Add routter for Blog

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+
+  <Navbar />
+
+  <div class="container">
+    <router-view/>
   </div>
-  <router-view/>
 </template>
+
+
+<script>
+import Navbar from './components/Navbar';
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
+
 
 <style>
 #app {
