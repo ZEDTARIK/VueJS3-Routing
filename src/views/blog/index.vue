324939e43d35e8db644251acd5807be1cd1e68d2
ETTARAK <ettarak.zouhair@gmail.com>
2021-01-18 18:25:33 +0100

    Coding, add bootswatch

--- src/views/blog/index.vue
+++ src/views/blog/index.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="jumbotron">
-    <h1 class="display-3">Hello, world!</h1>
+  <div class="jumbotron my-3">
+    <h1 class="display-3">{{jumMsg}}</h1>
     <p class="lead">
       This is a simple hero unit, a simple jumbotron-style component for calling
       extra attention to featured content or information.
-    </p>
-    <hr class="my-4" />
-    <p>
-      It uses utility classes for typography and spacing to space content out
-      within the larger container.
-    </p>
-    <p class="lead">
-      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      jumMsg: 'Lear vuejs 3'
+    }
+  }
+};
 </script>
 
 <style>
